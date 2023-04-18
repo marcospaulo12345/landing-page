6580f651a70efe373d7b39220e835b6676a54cf0
marcospaulo12345 <marcospaulo.mpf34@gmail.com>
2023-04-18 10:12:57 -0300

    Criado e testado o componente GriTwoColumns

---
+++ src/components/GridTwoColumns/mock.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'Grid two columns',
+  text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam nulla quibusdam saepe quae ratione id ea dolores assumenda iusto architecto culpa expedita odit laboriosam, officiis delectus, sint commodi mollitia exercitationem?`,
+  srcImage: 'assets/images/javascript.svg',
+};
